b3bf98bbd2981fffb583304214fd17a8ab46fe7a
kirkden <73966187+kirkden@users.noreply.github.com>
2022-07-04 22:34:34 +1000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,6 +8,7 @@ class AppLauncher(MycroftSkill):
     @intent_file_handler('launcher.app.intent')
     def handle_launcher_app(self, message):
         app = message.data.get('app')
+        self.log.info("This is an info level log message.")
 
         self.speak_dialog('launcher.app', data={
             'app': app
