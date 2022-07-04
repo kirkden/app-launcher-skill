db931a2c1ff33dcbcb8e7c5166408fbaf4614cea
Chris Kirkby <ckirkby@kirkden.com>
2022-07-04 22:25:48 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# <img src="https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/robot.svg" card_color="#22A7F0" width="50" height="50" style="vertical-align:bottom"/> App Launcher
+Start linux applications
+
+## About
+Start a linux application in the background
+
+## Examples
+* "Open {app}"
+* "Run {app}"
+* "Start {app}"
+
+## Credits
+Chris Kirkby
+
+## Category
+**Productivity**
+
+## Tags
+#Linux
+#Launcher
+
---
+++ __init__.py
@@ -0,0 +1,19 @@
+from mycroft import MycroftSkill, intent_file_handler
+
+
+class AppLauncher(MycroftSkill):
+    def __init__(self):
+        MycroftSkill.__init__(self)
+
+    @intent_file_handler('launcher.app.intent')
+    def handle_launcher_app(self, message):
+        app = message.data.get('app')
+
+        self.speak_dialog('launcher.app', data={
+            'app': app
+        })
+
+
+def create_skill():
+    return AppLauncher()
+
