ecc987b519a129bf4864141bbd0e565c8c8667f6
kirkden <73966187+kirkden@users.noreply.github.com>
2022-07-05 20:06:05 +1000

    Added subprocess

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+import subprocess
 
 class AppLauncher(MycroftSkill):
     def __init__(self):
@@ -8,12 +8,13 @@ class AppLauncher(MycroftSkill):
     @intent_file_handler('launcher.app.intent')
     def handle_launcher_app(self, message):
         app = message.data.get('app')
-        self.log.info("This is an info log message.")
-
 
         self.speak_dialog('launcher.app', data={
             'app': app
         })
+
+        self.log.info("Starting app %s", app)
+        aa = subprocess.Popen(app, shell=True)
 
 
 def create_skill():
