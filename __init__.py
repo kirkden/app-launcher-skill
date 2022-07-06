07049500d12d48026c3add77946cb1f01c162dcd
kirkden <73966187+kirkden@users.noreply.github.com>
2022-07-06 19:53:07 +1000

    Updated

--- __init__.py
+++ __init__.py
@@ -22,7 +22,7 @@ class AppLauncher(MycroftSkill):
             })
             subprocess.call("{} &".format(app), shell=True)
         else:
-            self.speak_dialog("launcher.app.not.exists", data={
+            self.speak_dialog('launcher.app.not.exists', data={
                 'app': app})
             print("Does not Exist")
 
